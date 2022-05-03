0a2bf933b8cdc27bf3db49706393c8d1a78f9ed3
cricklandia <cmstrickland@gmail.com>
2022-05-02 23:19:30 -0500

    1

---
+++ index.js
@@ -0,0 +1,62 @@
+//...the start a browser based game as seen on https://www.youtube.com/watch?v=vyqbNFMDRGQ&t=6562s
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+
+class Sprite{
+    constructor(position, velocity){
+        this.position = position
+        this.velocity = velocity
+    }
+
+    draw() {
+    c.fillStyle = 'blue' 
+    c.fillRect(this.position.x, this.position.y, 51, 151)
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+},
+   velocity: {
+       x:0,
+       y:0
+   }
+})
+
+
+player.draw()
+
+const enemy = new Sprite({
+    position: {
+    x: 900,
+    y: 100
+},
+    velocity: {
+        x:0,
+        y:0
+}
+})
+
+enemy.draw()
+
+
+console.log(player)
+
+console.log("canvas view loaded")
+
+function animate(){
+
+    window.requestAnimationFrame(animate)
+    console.log("animate() is working")
+}
+
+animate()
+
